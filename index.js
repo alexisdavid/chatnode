d9cf2c201da9de971a399463f7464ab256c03f05
alex duarte <alexisduarte1512@gmail.com>
2018-01-24 19:06:17 -0500

    primer comit del chat

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/publico/index.html');
+});
+app.use(express.static(__dirname + '/publico'));
+
+io.on('connection', function (socket) {
+  console.log("un usuario conectado")
+
+  socket.on('disconnect',function () {
+    console.log("usuario desconectado")
+  })
+  socket.on('mensaje', function (msg) {
+    console.log("un nuevo mensaje: ", msg);
+    io.emit('mensaje', msg);
+  })
+});
+
+
+
+http.listen(3000, function(){
+  console.log('Escuchando por el puerto localhost:3000');
+});
